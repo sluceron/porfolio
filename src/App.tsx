70f023af7a119aff27087ecdbe1e14ba918955f1
sluceron <sluceron@icloud.com>
2025-11-21 09:39:43 +0100

    Añadido header y footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/projects" component={Projects} />
         <Route path="/contact" component={Contact} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
